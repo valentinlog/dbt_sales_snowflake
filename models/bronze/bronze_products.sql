d8c1d9c276f8a7649ec7901d9725310e75c2b556
valentin.loghin@gmail.com <valentin.loghin@gmail.com>
2025-04-15 01:44:12 +0000

    added new source set

--- models/bronze/bronze_products.sql
+++ models/bronze/bronze_products.sql
@@ -1,4 +1,4 @@
 SELECT
 *
 FROM 
-{{ source('landing', 'products') }}
+{{ source('landing', 'bronze_products') }}
--- models/silver/silver_users.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    id,
-    created_at,
-    city,
-    state,
-    year(cast(birth_date as date)) as birth_year,
-    source as sales_channel
-FROM
-{{ ref('bronze_users') }}
